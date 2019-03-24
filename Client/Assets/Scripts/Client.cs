fa85182d467c092af66770e65bda61d8d2626970
aCastrooo <ant.castro94@gmail.com>
2019-03-24 16:07:31 -0400

    more

--- Client/Assets/Scripts/Client.cs
+++ Client/Assets/Scripts/Client.cs
@@ -43,7 +43,7 @@ public class Client : MonoBehaviour
         gameStarted = false;
         DontDestroyOnLoad(gameObject);
         //SERVER_IP = IPManager.GetIP(ADDRESSFAM.IPv4);
-        SERVER_IP = "24.191.198.127";
+        SERVER_IP = "172.19.235.241";
         Init();
         localPlayers = new HashSet<string>();
         startGameButton = GameObject.Find("StartGameButton").GetComponent<Button>();
